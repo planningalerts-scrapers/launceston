6baf682e79aa3f681a0844be8fe459873fdd6b5e
Daniel Caldeweyher <daniel@nexu.com.au>
2021-01-19 10:36:48 +1000

    launceston no longer has DA api. need to scrape html

--- scraper.rb
+++
@@ -1,3 +0,0 @@
-require File.join(File.dirname(__FILE__), "planningalerts_xml_feed", "planningalerts_xml_feed")
-
-PlanningAlertsXMLFeed.scrape("http://api.launceston.tas.gov.au/planning/planningAlerts.ashx")
